1803cb741619796f44f7654a032caca0803e07d8
Tomas Baran <tomas.baran@gmail.com>
2024-06-07 16:52:26 -0600

    feat: Add HomeScreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.pushNamed(context, '/settings');
+            },
+          ),
+        ],
+      ),
+      body:const Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
