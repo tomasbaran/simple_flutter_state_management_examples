7728fbeaedc1dbeeb069af25570d02a09fcb0cf7
Tomas Baran <tomas.baran@gmail.com>
2024-07-29 14:05:42 -0600

    feat: Load defaultSystemBrightness in the ProviderApp

--- lib/provider/provider_app.dart
+++ lib/provider/provider_app.dart
@@ -3,9 +3,14 @@ import 'package:provider/provider.dart';
 import 'package:simple_flutter_state_management_examples/provider/provider_app_home_screen.dart';
 import 'package:simple_flutter_state_management_examples/provider/provider_app_vm.dart';
 
-class ProviderApp extends StatelessWidget {
+class ProviderApp extends StatefulWidget {
   const ProviderApp({super.key});
 
+  @override
+  State<ProviderApp> createState() => _ProviderAppState();
+}
+
+class _ProviderAppState extends State<ProviderApp> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
--- lib/provider/provider_app_home_screen.dart
+++ lib/provider/provider_app_home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_flutter_state_management_examples/provider/provider_app_vm.dart';
 
@@ -23,10 +24,26 @@ class ProviderHomeScreen extends StatelessWidget {
   }
 }
 
-class DarkModeSwitch extends StatelessWidget {
+class DarkModeSwitch extends StatefulWidget {
   const DarkModeSwitch({
     super.key,
   });
+
+  @override
+  State<DarkModeSwitch> createState() => _DarkModeSwitchState();
+}
+
+class _DarkModeSwitchState extends State<DarkModeSwitch> {
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      final defaultSystemBrightness = SchedulerBinding.instance.platformDispatcher.platformBrightness;
+
+      Provider.of<ProviderAppVM>(context, listen: false)
+          .setAppBrightness(defaultSystemBrightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
